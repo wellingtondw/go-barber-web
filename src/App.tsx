3f259bb79a3145753d431846faebb5262eb98924
wellington <wellingtonslpbi@hotmail.com>
2020-08-22 17:36:39 -0300

    end login page style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-
+import SignIn from './pages/SignIn';
 const App: React.FC = () => {
   return (
     <>
-      <h1>App</h1>
+      <SignIn />
       <GlobalStyle />
     </>
   );
