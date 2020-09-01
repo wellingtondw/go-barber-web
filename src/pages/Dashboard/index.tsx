fb46b8275483ac235d5d0970795bddcd54b27392
wellington <wellingtonslpbi@hotmail.com>
2020-08-31 22:23:29 -0300

    next appointments style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiUser, FiClock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
@@ -16,7 +25,11 @@ const Dashboard: React.FC = () => {
           <img src={logoImg} alt="gobarber" />
 
           <Profile>
-            <img src={user.avatar_url} alt={user.name} />
+            {user.avatar_url ? (
+              <img src={user.avatar_url} alt={user.name} />
+            ) : (
+              <FiUser size={40} />
+            )}
             <div>
               <span>Bem-vindo,</span>
               <strong>{user.name}</strong>
@@ -28,6 +41,36 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+
+          <p>
+            <span>Hoje</span>
+            <span>Dia 6</span>
+            <span>Segunda feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+
+            <div>
+              <img
+                src="https://avatars3.githubusercontent.com/u/31135545?s=460&u=49990907a4746b348df6b752fd5067a23f9a3375&v=4"
+                alt="wellington"
+              />
+
+              <strong>Wellington Lima</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
